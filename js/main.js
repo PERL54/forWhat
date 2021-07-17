d8ece0f931fceaff87fb2366850b52b708c51334
PXRL <57463123+PERL54@users.noreply.github.com>
2021-07-18 01:44:20 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,50 @@
+function slowScroll(id) 
+{ 
+    var offset = 0;
+    $('html, body').animate({ 
+        scrollTop: $(id).offset().top - offset 
+    }, 1000);
+    return false; 
+}
+
+$(function()
+{
+
+        $("#typed").typed({
+            strings: [" the incredible.", " the beautiful.", " meaning."],
+            typeSpeed: 50,
+            backDelay: 1000,
+            startDelay: 500,
+            loop: true,
+            contentType: 'html',      
+        });
+
+}
+);
+
+jQuery(document).ready(function($) {
+  $(window).load(function() {
+    setTimeout(function() {
+      $('#preloader').fadeOut('slow', function() {});
+    }, 1500);
+
+  });
+});
+
+
+
+const pageWidth = document.documentElement.scrollWidth
+const pageHeight = document.documentElement.scrollHeight
+
+const windowInnerWidth = document.documentElement.clientWidth
+const windowInnerHeight = document.documentElement.clientHeight
+
+window.addEventListener('scroll', function() {
+    document.getElementById('current-scroll').style.width = ((window.pageYOffset * windowInnerWidth) / pageHeight).toString() + 'px';
+});
+
+$(document).ready(function()
+{
+    var $circle = $('.circle');
+    $circle.css('padding', 200)
+});
